895a41e32992e03b1597ea971d53fd12047d51c0
Mangel111111111 <mangel111111111@gmail.com>
2024-12-13 15:43:09 +0100

    feat/addRouteApi

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ToyController;
 
 Route::get('/elf', [ToyController::class,'index'])->name('apiindex');
+Route::delete('/elf/{id}',[ToyController::class,'destroy'])->name('apidestroy');
+Route::post('/elf',[ToyController::class,'store'])->name('apistore');
+Route::put('/elf/{id}',[ToyController::class,'update'])->name('apiupdate');
+Route::get('/elf/{id}', [ToyController::class, 'show'])->name('apishow');
