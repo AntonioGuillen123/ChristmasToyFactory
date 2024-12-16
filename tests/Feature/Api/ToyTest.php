88811a89da89040465454d9ba0d7f878f0f5ff10
Mangel111111111 <mangel111111111@gmail.com>
2024-12-16 09:50:45 +0100

    chore: add Class to ToyTest

--- tests/Feature/Api/ToyTest.php
+++ tests/Feature/Api/ToyTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Api;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Toy;
 
 class ToyTest extends TestCase
 {
